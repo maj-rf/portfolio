b6b590ec0363398fba886aa85cf4f1ed20bec792
bananabread08 <rmkfermin@gmail.com>
2022-02-23 11:53:12 +0800

    removed Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './components/Header';
 import Intro from './components/Intro';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -7,7 +6,6 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div>
-      <Header />
       <Intro />
       <Skills />
       <Projects />
--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header>
-      <div>Header</div>
-    </header>
-  );
-}
