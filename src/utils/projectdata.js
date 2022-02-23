e79d08dc2752850bbeb93bf3de8573f1beb85045
bananabread08 <rmkfermin@gmail.com>
2022-02-23 11:33:42 +0800

    added projectdata utils

---
+++ src/utils/projectdata.js
@@ -0,0 +1,52 @@
+export const projectdata = [
+  {
+    title: 'Lookr',
+    description: "A Where's Waldo-like Photo-Tagging App",
+    tags: ['React', 'styled-components', 'react-router', 'Firebase'],
+    image: '',
+    repo: 'https://github.com/bananabread08/photo-tagging-app',
+    live: 'https://photo-tagging-app-1fd39.web.app/#/',
+  },
+  {
+    title: 'Battleship',
+    description:
+      'A single player implementation of the classic Battleship game.',
+    tags: ['Javascript', 'HTML', 'CSS', 'Jest'],
+    image: '',
+    repo: 'https://github.com/bananabread08/battleship-vanilla',
+    live: 'https://bananabread08.github.io/battleship-vanilla/',
+  },
+  {
+    title: 'Shopping Cart',
+    description:
+      'A Frontend E-Commerce Site/App that captures the online shopping experience. ',
+    tags: ['React', 'styled-components', 'react-router'],
+    image: '',
+    repo: 'https://github.com/bananabread08/shopping-cart',
+    live: 'https://bananabread08.github.io/shopping-cart/',
+  },
+  {
+    title: 'CV Maker',
+    description: 'A React app to dynamically make your Resumes/CVs.',
+    tags: ['React', 'styled-components'],
+    image: '',
+    repo: 'https://github.com/bananabread08/cv-app',
+    live: 'https://bananabread08.github.io/cv-app/',
+  },
+  {
+    title: "Todon't",
+    description: 'A Task Management App (Todo-list)',
+    tags: ['JavaScript', 'HTML', 'CSS'],
+    image: '',
+    repo: 'https://github.com/bananabread08/todolist',
+    live: 'https://bananabread08.github.io/todolist/',
+  },
+  {
+    title: 'This Portfolio Site',
+    description: 'My Portfolio',
+    tags: ['React, styled-components'],
+    image: '',
+    repo: 'https://github.com/bananabread08/portfolio',
+    live: '',
+  },
+];
