2ca45d7a7228e3c5159ea46838c4f930bc81e103
bananabread08 <rmkfermin@gmail.com>
2022-04-30 15:51:01 +0800

    insert Datcord project data

--- src/utils/projectdata.js
+++ src/utils/projectdata.js
@@ -1,14 +1,35 @@
+import datcord from '../assets/Datcord.png';
 import lookr from '../assets/Lookr2.png';
 import battleship from '../assets/battleship.png';
 import switcher from '../assets/switcher.png';
-import ezcv from '../assets/ezcv.png';
 import todolist from '../assets/todolist.png';
 import myportfolio from '../assets/myportfolio.png';
 export const projectdata = [
   {
+    title: 'Datcord',
+    description: 'A One Server Discord Clone',
+    tags: [
+      'Fullstack',
+      'React',
+      'styled-components',
+      'react-router',
+      'Firebase',
+      'Jest',
+    ],
+    image: datcord,
+    repo: 'https://github.com/bananabread08/datcord',
+    live: 'https://bananabread08.github.io/datcord',
+  },
+  {
     title: 'Lookr',
     description: "A Where's Waldo-like Photo-Tagging App",
-    tags: ['React', 'styled-components', 'react-router', 'Firebase'],
+    tags: [
+      'Fullstack',
+      'React',
+      'styled-components',
+      'react-router',
+      'Firebase',
+    ],
     image: lookr,
     repo: 'https://github.com/bananabread08/photo-tagging-app',
     live: 'https://photo-tagging-app-1fd39.web.app/#/',
@@ -17,7 +38,7 @@ export const projectdata = [
     title: 'Battleship',
     description:
       'A single player implementation of the classic Battleship game.',
-    tags: ['JavaScript', 'HTML', 'CSS', 'Jest'],
+    tags: ['Frontend', 'JavaScript', 'HTML', 'CSS', 'Jest'],
     image: battleship,
     repo: 'https://github.com/bananabread08/battleship-vanilla',
     live: 'https://bananabread08.github.io/battleship-vanilla/',
@@ -25,24 +46,16 @@ export const projectdata = [
   {
     title: 'Shopping Cart',
     description:
-      'A Frontend E-Commerce Site/App that captures the online shopping experience. ',
-    tags: ['React', 'styled-components', 'react-router'],
+      'An E-Commerce Site/App that captures the online shopping experience. ',
+    tags: ['Frontend', 'React', 'styled-components', 'react-router'],
     image: switcher,
     repo: 'https://github.com/bananabread08/shopping-cart',
     live: 'https://bananabread08.github.io/shopping-cart/',
   },
   {
-    title: 'CV Maker',
-    description: 'A React app to dynamically make your Resumes/CVs.',
-    tags: ['React', 'styled-components'],
-    image: ezcv,
-    repo: 'https://github.com/bananabread08/cv-app',
-    live: 'https://bananabread08.github.io/cv-app/',
-  },
-  {
     title: "Todon't",
     description: 'A Task Management App (Todo-list)',
-    tags: ['JavaScript', 'HTML', 'CSS'],
+    tags: ['Frontend', 'JavaScript', 'HTML', 'CSS'],
     image: todolist,
     repo: 'https://github.com/bananabread08/todolist',
     live: 'https://bananabread08.github.io/todolist/',
@@ -50,7 +63,7 @@ export const projectdata = [
   {
     title: 'This Portfolio Site',
     description: 'My Portfolio',
-    tags: ['React', 'styled-components'],
+    tags: ['Frontend', 'React', 'styled-components'],
     image: myportfolio,
     repo: 'https://github.com/bananabread08/portfolio',
     live: 'https://bananabread08.github.io/portfolio/',
