0e4040aff1d94ecc58a060764aea743b1dca2cf4
bananabread08 <rmkfermin@gmail.com>
2022-09-23 09:41:10 +0800

    adjust overall Layout spacing

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,10 +1,10 @@
 import AppHead from './Apphead';
 import { Footer } from './Footer';
 import Header from './Header';
-
+import styles from '../../styles/Layout.module.scss';
 export default function Layout({ children }: { children: JSX.Element }) {
   return (
-    <div>
+    <div className={styles.container}>
       <AppHead title="maj.dev" content="Maj's Portfolio" />
       <Header />
       {children}
