bb75c48181581b9ffa772f25ac623f77bbc53e85
bananabread08 <rmkfermin@gmail.com>
2022-09-26 17:31:20 +0800

    add Tags, wrap a tags with buttons

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { projectdata } from '../../components/data/projectData';
 import { GetStaticProps } from 'next';
 import { projectType } from '../../components/types/projectTypes';
-
+import { Tags } from '../../components/project/Tags';
 type Props = {
   proj: projectType;
 };
@@ -39,13 +39,22 @@ const ProjectPage = ({ proj }: Props) => {
       <h1>{proj.title}</h1>
       <Image src={proj.image} alt={proj.title} />
       <p>{proj.description}</p>
+      <ul>
+        {proj.tags.map((tag) => {
+          return <Tags key={tag + proj.title} tag={tag} />;
+        })}
+      </ul>
       <div>
-        <a target="_blank" href={proj.repo} rel="noopener noreferrer">
-          Github
-        </a>
-        <a target="_blank" href={proj.live} rel="noopener noreferrer">
-          Demo
-        </a>
+        <button>
+          <a target="_blank" href={proj.repo} rel="noopener noreferrer">
+            Code
+          </a>
+        </button>
+        <button>
+          <a target="_blank" href={proj.live} rel="noopener noreferrer">
+            Demo
+          </a>
+        </button>
       </div>
       <button onClick={() => router.back()}>Back to Home</button>
     </div>
