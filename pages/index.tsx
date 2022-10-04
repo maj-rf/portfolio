e6daf07a44db82e0d2cff47c3f9c2232df7e7946
bananabread08 <rmkfermin@gmail.com>
2022-10-04 10:09:58 +0800

    import About and Hero to Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,13 @@
 import type { NextPage } from 'next';
-import styles from '../styles/Home.module.scss';
 import { Stack } from '../components/Stack';
 import { Projects } from '../components/project/Projects';
+import { Hero } from '../components/Hero';
+import { About } from '../components/About';
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Maj&#39;s Portfolio!</a>
-        </h1>
-        <section className={styles.container}>
-          <h1>Software Engineer, Front-end Developer, Gamer</h1>
-          <p>
-            I am Maj, a software developer based in the Philippines. I like to
-            teach myself how to code. I am passionate about coding and I am
-            constantly learning more about web development. I went through the
-            non-traditional way of learning programming: self-teaching. I am
-            confident with my learning ability and with that, I love trying new
-            technologies.
-          </p>
-          <p>I usually catch Pokemon during my free time!</p>
-        </section>
-      </main>
+    <div>
+      <Hero />
+      <About />
       <Stack />
       <Projects />
     </div>
