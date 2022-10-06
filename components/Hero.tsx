58d399f6665194380be154c9392d0e4f6c594038
bananabread08 <rmkfermin@gmail.com>
2022-10-06 10:34:14 +0800

    create typewriter effect in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,32 @@
 import Link from 'next/link';
 import { FaDog } from 'react-icons/fa';
+
 import styles from '../styles/Hero.module.scss';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
 export const Hero = () => {
+  const [text] = useTypewriter({
+    words: ['Hi, I am Maj!', 'Software Developer', 'Pokemon Trainer'],
+    loop: true,
+    delaySpeed: 2000,
+    typeSpeed: 75,
+  });
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>
-        Hi, I am Maj! Software Developer. <FaDog />
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+        <FaDog />
       </h1>
-      <Link href="#about" passHref>
-        <button
-          className={styles.scroll_btn}
-          aria-label="sroll button"
-        ></button>
-      </Link>
+
+      <div className={styles.btn_container}>
+        <Link href="#about" passHref>
+          <button
+            className={styles.scroll_btn}
+            aria-label="sroll button"
+          ></button>
+        </Link>
+      </div>
     </main>
   );
 };
