a15d347542f9a78fcd937efa7421dde1d04e53e1
bananabread08 <rmkfermin@gmail.com>
2022-10-12 10:56:03 +0800

    remove motion.div in _app and put it in projects/[id]

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import AppHead from './Apphead';
 import { Footer } from './Footer';
 import Header from './Header';
 import styles from '../../styles/Layout.module.scss';
-export default function Layout({ children }: { children: JSX.Element }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.container}>
       <AppHead title="maj.dev" content="Maj's Portfolio" />
--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -7,6 +7,8 @@ import { Tags } from '../../components/project/Tags';
 import styles from '../../styles/ProjectPage.module.scss';
 import { HiArrowCircleLeft } from 'react-icons/hi';
 import { AiOutlineEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 type Props = {
   proj: projectType;
 };
@@ -35,40 +37,61 @@ export const getStaticProps: GetStaticProps = async (context) => {
 };
 
 const ProjectPage = ({ proj }: Props) => {
+  const router = useRouter();
   return (
-    <section className={styles.container}>
-      <div className={styles.heading_container}>
-        <Link href="/" scroll={false}>
-          <a className={styles.back}>
-            <HiArrowCircleLeft size={30} />
-          </a>
-        </Link>
+    <motion.div
+      key={router.route}
+      initial="initialState"
+      animate="animateState"
+      exit="exitState"
+      variants={{
+        initialState: {
+          opacity: 0,
+          x: -200,
+          y: 0,
+        },
+        animateState: {
+          opacity: 1,
+          x: 0,
+          y: 0,
+        },
+        exitState: { opacity: 0, x: 0, y: -100 },
+      }}
+    >
+      <section className={styles.container}>
+        <div className={styles.heading_container}>
+          <Link href="/" scroll={false}>
+            <a className={styles.back}>
+              <HiArrowCircleLeft size={30} />
+            </a>
+          </Link>
 
-        <h1>{proj.title}</h1>
-      </div>
-      <Image src={proj.image} alt={proj.title} />
-      <ul className={styles.tag_container}>
-        {proj.tags.map((tag) => {
-          return <Tags key={tag + proj.title} tag={tag} />;
-        })}
-      </ul>
-      <p>{proj.description}</p>
-      <div className={styles.links}>
-        <div className={styles.link}>
-          <a target="_blank" href={proj.repo} rel="noopener noreferrer">
-            <AiOutlineEye />
-            Code
-          </a>
+          <h1>{proj.title}</h1>
         </div>
+        <Image src={proj.image} alt={proj.title} />
+        <ul className={styles.tag_container}>
+          {proj.tags.map((tag) => {
+            return <Tags key={tag + proj.title} tag={tag} />;
+          })}
+        </ul>
+        <p>{proj.description}</p>
+        <div className={styles.links}>
+          <div className={styles.link}>
+            <a target="_blank" href={proj.repo} rel="noopener noreferrer">
+              <AiOutlineEye />
+              Code
+            </a>
+          </div>
 
-        <div className={styles.link}>
-          <a target="_blank" href={proj.live} rel="noopener noreferrer">
-            <AiFillGithub />
-            Demo
-          </a>
+          <div className={styles.link}>
+            <a target="_blank" href={proj.live} rel="noopener noreferrer">
+              <AiFillGithub />
+              Demo
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </motion.div>
   );
 };
 
