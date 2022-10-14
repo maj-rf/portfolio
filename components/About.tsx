1623e5ae4d945adb7e288247b07a2ed450ff1ea2
bananabread08 <rmkfermin@gmail.com>
2022-10-14 10:31:31 +0800

    fix About Me font size

--- components/About.tsx
+++ components/About.tsx
@@ -4,21 +4,21 @@ export const About = () => {
   return (
     <section className={utils.container} id="about">
       <h2>About Me</h2>
-      <p style={{ textAlign: 'left' }}>
+      <p style={{ textAlign: 'left', fontSize: 18 }}>
         I am a software developer based in the Philippines. I am passionate
         about coding and I am constantly learning more about web development. I
         am confident with my learning ability and with that, I love trying new
         technologies.
       </p>
-      <p style={{ textAlign: 'left' }}>
+      <p style={{ textAlign: 'left', fontSize: 18 }}>
         I usually play video games during my free time. Mostly catching Pokemon!
       </p>
-      <div>
+      <div style={{ margin: '1rem' }}>
         <a href="https://github.com/bananabread08">
-          <AiFillGithub size={70} />
+          <AiFillGithub size={50} />
         </a>
         <a href="https://www.linkedin.com/in/ralph-majed-keene-fermin-601575231/">
-          <AiFillLinkedin size={70} />
+          <AiFillLinkedin size={50} />
         </a>
       </div>
     </section>
